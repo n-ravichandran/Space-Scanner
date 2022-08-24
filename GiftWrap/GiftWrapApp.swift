ae7f1758dad13b79ea67d38370df5a8c847e27fd
Niranjan Ravichandran <niranjan.ravichandran@latch.com>
2022-08-24 19:35:02 +0530

    Initial Commit

---
+++ GiftWrap/GiftWrapApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftWrapApp.swift
+//  GiftWrap
+//
+//  Created by Niranjan Ravichandran on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct GiftWrapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
